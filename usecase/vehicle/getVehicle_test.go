e2c820ae5f58f3cc894d5a8d60eede30bc3c6204
Ion Dodon <ion.dodon@amdaris.com>
2025-01-09 19:10:32 +0200

    added mockery

--- usecase/vehicle/getVehicle_test.go
+++ usecase/vehicle/getVehicle_test.go
@@ -6,19 +6,15 @@ import (
 	"github.com/iondodon/go-vbs/domain"
 	vehRepo "github.com/iondodon/go-vbs/repository/vehicle"
 
-	uuidLib "github.com/google/uuid"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"go.uber.org/mock/gomock"
 )
 
 func Test_getVehicle_ByUUID(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockVehRepo := vehRepo.NewMockVehicleRepository(ctrl)
+	mockVehRepo := vehRepo.NewMockVehicleRepository(t)
 	var gvuc GetVehicle = &getVehicle{vehicleRepository: mockVehRepo}
 
-	uuid, err := uuidLib.Parse("c2df2b03-92e8-41ad-9a74-0b7b040a4cf5")
+	uuid, err := uuid.Parse("c2df2b03-92e8-41ad-9a74-0b7b040a4cf5")
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err.Error())
 	}
