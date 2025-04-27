56ad01718fa967c3ae3c4c4f55eb2271d84dbbbe
Ion Dodon <ion.dodon@amdaris.com>
2025-04-27 14:35:19 +0300

    hide certain files

--- usecase/vehicle/getVehicle_test.go
+++ usecase/vehicle/getVehicle_test.go
@@ -1,34 +1,34 @@
 package vehicle
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"context"
+	"testing"
 
-// 	"github.com/iondodon/go-vbs/domain"
-// 	vehRepo "github.com/iondodon/go-vbs/repository/vehicle"
+	"github.com/iondodon/go-vbs/domain"
+	vehRepo "github.com/iondodon/go-vbs/repository/vehicle"
 
-// 	"github.com/google/uuid"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
 
-// func Test_getVehicle_ByUUID(t *testing.T) {
-// 	mockVehRepo := vehRepo.NewMockVehicleRepository(t)
-// 	var gvuc GetVehicle = GetVehicle{vehicleRepository: mockVehRepo}
+func Test_getVehicle_ByUUID(t *testing.T) {
+	mockVehRepo := vehRepo.NewMockVehicleRepository(t)
+	var gvuc GetVehicle = &getVehicle{vehicleRepository: mockVehRepo}
 
-// 	uuid, err := uuid.Parse("c2df2b03-92e8-41ad-9a74-0b7b040a4cf5")
-// 	if err != nil {
-// 		t.Fatalf("unexpected error: %s", err.Error())
-// 	}
+	uuid, err := uuid.Parse("c2df2b03-92e8-41ad-9a74-0b7b040a4cf5")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
 
-// 	expectedVeh := &domain.Vehicle{}
+	expectedVeh := &domain.Vehicle{}
 
-// 	t.Run("the repository is called with the correct UUID", func(t *testing.T) {
-// 		mockVehRepo.EXPECT().FindByUUID(context.Background(), uuid).Return(expectedVeh, nil).Times(1)
+	t.Run("the repository is called with the correct UUID", func(t *testing.T) {
+		mockVehRepo.EXPECT().FindByUUID(context.Background(), uuid).Return(expectedVeh, nil).Times(1)
 
-// 		veh, err := gvuc.ByUUID(context.Background(), uuid)
+		veh, err := gvuc.ByUUID(context.Background(), uuid)
 
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, expectedVeh, veh)
-// 	})
+		assert.Nil(t, err)
+		assert.Equal(t, expectedVeh, veh)
+	})
 
-// }
+}
